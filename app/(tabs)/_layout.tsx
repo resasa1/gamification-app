72b9adb7bdf7150fa2d52efa53f17c7e9fad2e61
Resa Budi Prasetyo <120000539+resasa1@users.noreply.github.com>
2024-12-14 17:05:15 +0700

    styling background nav

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout(){
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: '#ffd33d',
+                headerStyle: {
+                    backgroundColor: '#25292e',
+                },
+                headerShadowVisible: false,
+                headerTintColor: '#fff',
+
+                tabBarStyle: {
+                    backgroundColor: '#25292e',
+                },
+            }}
+        >
+            <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+            ) }} />
+            <Tabs.Screen name="about" options={{ title: 'About' }} />
+        </Tabs>
+    )
+}
